175d31672647832ca8f3f760610cdd873c32287a
kjw-blog <rkdwjddnr11@naver.com>
2024-01-10 23:38:25 +0900

    feat: 숫자야구
    - state 추가
    - 반복문 임시 추가

--- 3.숫자야구/NumberBaseball.jsx
+++ 3.숫자야구/NumberBaseball.jsx
@@ -1,8 +1,43 @@
 import React, { Component } from 'react';
 
+function getNumbers() {}
+
 class NumberBaseball extends Component {
+  state = {
+    result: '',
+    value: '',
+    answer: getNumbers(),
+    tries: [],
+  };
+
+  onSubmitForm = (e) => {
+    e.preventDefault();
+  };
+
+  onChangeInput = (e) => {
+    this.setState({ value: e.target.value });
+  };
+
   render() {
-    return <>test</>;
+    return (
+      <>
+        <h1>{this.state.result}</h1>
+        <form onSubmit={this.onSubmitForm}>
+          <input
+            maxLength={4}
+            value={this.state.value}
+            onChange={this.onChangeInput}
+          />
+          <button></button>
+        </form>
+        <div>시도: {this.state.tries.length}</div>
+        <ul>
+          {['사과', '바나나', '포도', '귤', '감', '배', '밤'].map((v) => {
+            return <li>{v}</li>;
+          })}
+        </ul>
+      </>
+    );
   }
 }
 
