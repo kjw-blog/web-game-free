6b16fdd45ac0692a7606616e3d9a60e39ee5e7b0
kjw-blog <rkdwjddnr11@naver.com>
2024-01-11 22:42:33 +0900

    feat: 숫자야구
    - 숫자야구 만들기

--- 3.숫자야구/Try.jsx
+++ 3.숫자야구/Try.jsx
@@ -4,10 +4,8 @@ class Try extends Component {
   render() {
     return (
       <li>
-        <b>{this.props.value.fruit}</b> - <span>{this.props.value.taste}</span>
-        <div>컨텐츠 1</div>
-        <div>컨텐츠 2</div>
-        <div>컨텐츠 3</div>
+        <div>{this.props.tryInfo.try}</div>
+        <div>{this.props.tryInfo.result}</div>
       </li>
     );
   }
