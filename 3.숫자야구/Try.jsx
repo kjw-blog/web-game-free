d75d15a1f940fdf8cb03d4a3de76545c29dfee44
kjw-blog <rkdwjddnr11@naver.com>
2024-01-11 23:50:53 +0900

    feat: 숫자 야구
    - props를 변경해야 하는 상황 대처법

--- 3.숫자야구/Try.jsx
+++ 3.숫자야구/Try.jsx
@@ -1,13 +1,22 @@
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
 
 // 클래스 컴포넌트의 PureComponent의 역할을 memo가 해준다.
 // 부모컴포넌트가 변경될 떄 memo를 적용한 자식컴포넌트의 props가 변하지 않으면
 // 해당 컴포넌트는 리렌더링 되지 않는다.
 const Try = memo(({ tryInfo }) => {
+  // props의 값은 직접 변경할 수 없고, 부모 컴포넌트에서 변경해줘야한다.
+  // tryInfo.try = 'hello'
+
+  // 불가피하게 바꿔줘야할 때
+  const [result, setResult] = useState(tryInfo.result);
+
+  const onClick = () => {
+    setResult('1');
+  };
   return (
     <li>
       <div>{tryInfo.try}</div>
-      <div>{tryInfo.result}</div>
+      <div onClick={onClick}>{result}</div>
     </li>
   );
 });
