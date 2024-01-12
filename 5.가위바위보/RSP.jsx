80e780dc7e637e6af1951f948984230dbea31842
kjw-blog <rkdwjddnr11@naver.com>
2024-01-12 20:21:28 +0900

    feat: 가위바위보
    - 라이프사이클와 setInterval을 이용한 이미지 위치 변경

--- 5.가위바위보/RSP.jsx
+++ 5.가위바위보/RSP.jsx
@@ -5,21 +5,59 @@ import React, { Component } from 'react';
 // (state/props 바뀔 때 -> shouldComponentUpdate(true) -> render -> componentDidUpdate)
 // (부모 컴포넌트에서 해당 컴포넌트가 사라질 때 -> componentWillUnmount) -> 소멸
 
+const rspCoords = {
+  바위: '0',
+  가위: '-142px',
+  보: '-284px',
+};
+
+const scores = {
+  가위: 1,
+  바위: 0,
+  보: -1,
+};
+
 class RSP extends Component {
   state = {
     result: '',
     score: 0,
-    imgCoord: 0,
+    imgCoord: '0',
   };
 
-  // 처음 render가 실행돼서 JSX문이 DOM에 붙었을 때 실행
-  componentDidMount() {}
+  interval;
+
+  // 처음 render가 실행돼서 JSX문이 DOM에 붙었을 때 실행, 비동기 요청을 많이 함
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      const { imgCoord } = this.state;
+
+      if (imgCoord === rspCoords.바위) {
+        this.setState({
+          imgCoord: rspCoords.가위,
+        });
+      } else if (imgCoord === rspCoords.가위) {
+        this.setState({
+          imgCoord: rspCoords.보,
+        });
+      } else if (imgCoord === rspCoords.보) {
+        this.setState({
+          imgCoord: rspCoords.바위,
+        });
+      }
+    }, 500);
+  }
 
   // 리렌더링 후에 실행
   componentDidUpdate() {}
 
-  // 컴포넌트가 제거되기 전에 실행
-  componentWillUnmount() {}
+  // 컴포넌트가 제거되기 전에 실행, 비동기 요청 정리를 많이 함
+  componentWillUnmount() {
+    // 컴포넌트가 없어질 때 완료되지 않은 비동기 요청을 처리해주지 않으면
+    // 메모리 누수가 생긴다.
+    clearInterval(this.interval);
+  }
+
+  onClickBtn = (choice) => {};
 
   render() {
     const { result, score, imgCoord } = this.state;
@@ -33,17 +71,25 @@ class RSP extends Component {
           }}
         ></div>
         <div>
-          <button id="rock" className="btn" onClick={() => onClickBtn('바위')}>
+          <button
+            id="rock"
+            className="btn"
+            onClick={() => this.onClickBtn('바위')}
+          >
             바위
           </button>
           <button
             id="scissor"
             className="btn"
-            onClick={() => onClickBtn('가위')}
+            onClick={() => this.onClickBtn('가위')}
           >
             가위
           </button>
-          <button id="paper" className="btn" onClick={() => onClickBtn('보')}>
+          <button
+            id="paper"
+            className="btn"
+            onClick={() => this.onClickBtn('보')}
+          >
             보
           </button>
         </div>
