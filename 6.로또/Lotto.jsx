6ba47591b86caa69414a2d72d2f1d9556a0861e1
kjw-blog <rkdwjddnr11@naver.com>
2024-01-15 21:17:15 +0900

    feat: 로또 추첨기
    - 메모이제이션 hooks를 사용해서 성능 최적화

--- 6.로또/Lotto.jsx
+++ 6.로또/Lotto.jsx
@@ -1,4 +1,10 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 import Ball from './Ball';
 
 function getWinNumbers() {
@@ -20,21 +26,29 @@ function getWinNumbers() {
 }
 
 const Lotto = () => {
-  const [winNumbers, setWinNumbers] = useState(getWinNumbers);
+  // 계산식이 복잡한 값은 useMemo로 메모이제이션 할 수 있다.
+  // 리렌더링 될 때 dependencies에 값이 변경되지 않으면 값을 새로 계산하지 않고 저장해둔걸 사용함
+  const lottoNumbers = useMemo(() => getWinNumbers(), []);
+
   const [winBalls, setWinBalls] = useState([]);
+  const [winNumbers, setWinNumbers] = useState(lottoNumbers);
+  // const [winNumbers, setWinNumbers] = useState(getWinNumber); // 이렇게 써도 useMemo와 같은 동작을 기대할 수 잇음
   const [bonus, setBonus] = useState(null);
   const [redo, setRedo] = useState(false);
 
   const timeouts = useRef([]);
 
-  const onClickRedo = () => {
+  // useCallback으로 함수 자체를 메모이제이션 할 수 있다.
+  // 리렌더링 될 때 dependencies에 값이 변경되지 않으면 함수를 새로 만들지 않고 저장해둔걸 사용함
+  // 자식 컴포넌트에 해당 함수를 props로 넘길 때 useCallback을 꼭 해줘야한다.
+  const onClickRedo = useCallback(() => {
     setWinNumbers(getWinNumbers());
     setWinBalls([]);
     setBonus(null);
     setRedo(false);
 
     timeouts.current = [];
-  };
+  }, [winNumbers]);
 
   useEffect(() => {
     for (let i = 0; i < winNumbers.length - 1; i++) {
