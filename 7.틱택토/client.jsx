f95e8739b5fef0672c726005c1594c467e280d25
kjw-blog <rkdwjddnr11@naver.com>
2024-01-16 22:25:31 +0900

    feat: 틱택토
    - 틱택토 컴포넌트 추가
    - useReducer 사용법

---
+++ 7.틱택토/client.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TicTacToe from './TicTacToe';
+
+ReactDOM.createRoot(document.querySelector('#root')).render(<TicTacToe />);
