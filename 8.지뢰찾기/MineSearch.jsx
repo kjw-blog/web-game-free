bccb2c8e0ed748807c3ca32559fe48f7343ef4c7
kjw-blog <rkdwjddnr11@naver.com>
2024-01-17 21:48:21 +0900

    feat: 지뢰찾기
    - 주변 칸 지뢰 검사 로직 추가

--- 8.지뢰찾기/MineSearch.jsx
+++ 8.지뢰찾기/MineSearch.jsx
@@ -27,7 +27,6 @@ const initialState = {
 };
 
 const plantMine = (row, cell, mine) => {
-  console.log(row, cell, mine);
   const candidate = Array(row * cell)
     .fill()
     .map((arr, i) => {
@@ -83,6 +82,39 @@ const reducer = (state, action) => {
       const tableData = [...state.tableData];
       tableData[action.row] = [...state.tableData[action.row]];
       tableData[action.row][action.cell] = CODE.OPENED;
+
+      /** 주변 칸 검사 로직 */
+      let around = [];
+
+      if (tableData[action.row - 1]) {
+        // 첫번째 줄이 아닐 때
+        around = around.concat(
+          tableData[action.row - 1][action.cell - 1],
+          tableData[action.row - 1][action.cell],
+          tableData[action.row - 1][action.cell + 1]
+        );
+      }
+      around = around.concat(
+        tableData[action.row][action.cell - 1],
+        tableData[action.row][action.cell + 1]
+      );
+
+      if (tableData[action.row + 1]) {
+        // 마지막 줄이 아닐 때
+        around = around.concat(
+          tableData[action.row + 1][action.cell - 1],
+          tableData[action.row + 1][action.cell],
+          tableData[action.row + 1][action.cell + 1]
+        );
+      }
+
+      const count = around.filter((v) =>
+        [CODE.MINE, CODE.FLAG_MINE, CODE.QUESTION_MINE].includes(v)
+      ).length;
+
+      tableData[action.row][action.cell] = count;
+
+      /** 주변 칸 검사 로직 */
 
       return { ...state, tableData };
     }
--- 8.지뢰찾기/Td.jsx
+++ 8.지뢰찾기/Td.jsx
@@ -51,7 +51,7 @@ const getTdText = (code) => {
     case CODE.QUESTION:
       return '?';
     default:
-      return '';
+      return code || '';
   }
 };
 
