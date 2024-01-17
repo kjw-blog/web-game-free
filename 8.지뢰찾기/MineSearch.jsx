452b4de78b8c8789858d739fb19387cf4ae9bbfb
kjw-blog <rkdwjddnr11@naver.com>
2024-01-17 16:36:18 +0900

    feat: 지뢰찾기
    - 지뢰찾기 context 설정

--- 8.지뢰찾기/MineSearch.jsx
+++ 8.지뢰찾기/MineSearch.jsx
@@ -1,6 +1,22 @@
-import React, { useReducer } from 'react';
+import React, { createContext, useMemo, useReducer } from 'react';
 import Table from './Table';
 import Form from './Form';
+
+export const CODE = {
+  MINE: -7,
+  NORMAL: -1,
+  QUESTION: -2,
+  FLAG: -3,
+  QUESTION_MINE: -4,
+  FLAG_MINE: -5,
+  CLICKED_MINE: -6,
+  OPENED: 0, // 0 이상이면 다 opened
+};
+
+export const TableContext = createContext({
+  tableData: [],
+  dispatch: () => {},
+});
 
 const initialState = {
   tableData: [],
@@ -8,8 +24,15 @@ const initialState = {
   result: '',
 };
 
+export const START_GAME = 'START_GAME';
+
 const reducer = (state, action) => {
   switch (action.type) {
+    case START_GAME:
+      return {
+        ...state,
+        tableData: plantMine(action.row, action.cell, action.mine),
+      };
     default:
       return state;
   }
@@ -18,13 +41,18 @@ const reducer = (state, action) => {
 const MineSearch = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  const value = useMemo(
+    () => ({ tableData: state.tableData, dispatch }),
+    [state.tableData]
+  );
+
   return (
-    <>
+    <TableContext.Provider value={value}>
       <Form />
       <div>{state.timer}</div>
       <Table />
       <div>{state.result}</div>
-    </>
+    </TableContext.Provider>
   );
 };
 
